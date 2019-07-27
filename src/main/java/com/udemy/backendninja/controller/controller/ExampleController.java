b4e06151e4ce3c63cc653a7457ecdd04f6eae66a
Sebastian Posadas <posadas.sca@gmail.com>
2019-07-27 16:55:11 -0300

    Insertando datos encapsulados en clase Persona dentro de la vista example

--- src/main/java/com/udemy/backendninja/controller/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package com.udemy.backendninja.controller.controller;
 
 import com.udemy.backendninja.controller.Constante;
+import com.udemy.backendninja.controller.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +15,14 @@ public class ExampleController {
 
     @GetMapping("/exampleString")
     public String obtenerPaginaExampleC(Model model){
-        model.addAttribute("name", "Sebastian");
+        model.addAttribute("person", new Person("Sebastian", 24));
         return Constante.EXAMPLE_HTML;
     }
 
     @GetMapping("/exampleMAV")
     public ModelAndView obtenerPaginaExampleMAV(){
         ModelAndView mav = new ModelAndView(Constante.EXAMPLE_HTML);
-        mav.addObject("name", "MikeMAV");
+        mav.addObject("person", new Person("MikeMAV", 30));
         return mav;
     }
 }
