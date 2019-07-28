83054cb91cca6c8983cc14b70f189cccef0cb116
Sebastian Posadas <posadas.sca@gmail.com>
2019-07-28 00:44:08 -0300

    Iteracion de ArrayList con Thymeleaf. Atributo th:each

--- src/main/java/com/udemy/backendninja/controller/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/controller/ExampleController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/example")
@@ -15,14 +18,27 @@ public class ExampleController {
 
     @GetMapping("/exampleString")
     public String obtenerPaginaExampleC(Model model){
-        model.addAttribute("person", new Person("Sebastian", 24));
+        model.addAttribute("people", getPeople());
         return Constante.EXAMPLE_HTML;
     }
 
     @GetMapping("/exampleMAV")
     public ModelAndView obtenerPaginaExampleMAV(){
         ModelAndView mav = new ModelAndView(Constante.EXAMPLE_HTML);
-        mav.addObject("person", new Person("MikeMAV", 30));
+        mav.addObject("people", getPeople());
         return mav;
     }
+
+    private List<Person> getPeople() {
+        List<Person> people = new ArrayList<Person>();
+        people.add(new Person("Simon", 0));
+        people.add(new Person("Cristian", 15));
+        people.add(new Person("Sebastian", 24));
+        people.add(new Person("Andres", 19));
+        people.add(new Person("Carolina", 28));
+
+        return people;
+    }
+
+
 }
