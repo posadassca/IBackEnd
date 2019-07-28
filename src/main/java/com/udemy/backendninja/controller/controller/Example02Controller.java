de094816f2efc3cfc3dc81ad257e1d69a2f6d3fc
Sebastian Posadas <posadas.sca@gmail.com>
2019-07-28 01:28:10 -0300

    Otro tipo de peticion GET. A través del QueryString.

--- src/main/java/com/udemy/backendninja/controller/controller/Example02Controller.java
+++ src/main/java/com/udemy/backendninja/controller/controller/Example02Controller.java
@@ -3,6 +3,7 @@ package com.udemy.backendninja.controller.controller;
 import com.udemy.backendninja.controller.Constante;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,6 +18,15 @@ public class Example02Controller {
     //localhost:8080/example02/request01?nm=John
     @GetMapping("/request01")
     public ModelAndView request01(@RequestParam(name="nm", required = false, defaultValue = "NULL") String name){
+        ModelAndView mav = new ModelAndView(Constante.EXAMPLE02_HTML);
+        mav.addObject("nm_in_model", name);
+        return mav;
+    }
+
+    //localhost:8080/example02/request02/John
+    //localhost:8080/example02/request02/Mikel
+    @GetMapping("/request02/{nm}")
+    public ModelAndView request02(@PathVariable("nm") String name){
         ModelAndView mav = new ModelAndView(Constante.EXAMPLE02_HTML);
         mav.addObject("nm_in_model", name);
         return mav;
