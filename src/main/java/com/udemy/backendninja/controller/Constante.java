3f2fc15480e51cf3a1906f5ed23dcec3c2e9ef1d
Sebastian Posadas <posadas.sca@gmail.com>
2019-07-29 21:19:19 -0300

    Ejemplo, correccion de paths relativos y absolutos

--- src/main/java/com/udemy/backendninja/controller/Constante.java
+++ src/main/java/com/udemy/backendninja/controller/Constante.java
@@ -6,5 +6,7 @@ public class Constante {
     public static final String EXAMPLE02_HTML = "example02";
     public static final String FORM_VIEW = "form";
     public static final String RESULT_VIEW = "result";
+    public static final String EXAMPLE04_ERROR = "404";
+    public static final String EXAMPLE04_STATUS = "500";
 
 }
