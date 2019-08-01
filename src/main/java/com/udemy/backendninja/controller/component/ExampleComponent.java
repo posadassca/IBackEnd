2dfbea65dd11fe9d33d3c8b5fe8aa8b5ac6478de
Sebastian Posadas <posadas.sca@gmail.com>
2019-08-01 15:59:07 -0300

    Genero primer repository para entidad Course. Y genero varias querys generadas con Spring.

--- src/main/java/com/udemy/backendninja/controller/component/ExampleComponent.java
+++ src/main/java/com/udemy/backendninja/controller/component/ExampleComponent.java
@@ -1,7 +1,11 @@
 package com.udemy.backendninja.controller.component;
 
+import com.udemy.backendninja.controller.entity.Course;
+import com.udemy.backendninja.controller.repository.CourseJpaRepository;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("exampleComponent")
@@ -9,7 +13,12 @@ public class ExampleComponent {
 
     private static final Log LOGGER = LogFactory.getLog(ExampleComponent.class);
 
+    @Autowired
+    @Qualifier("courseJpaRepository")
+    private CourseJpaRepository courseJpaRepository;
+
     public void sayHello(){
+        courseJpaRepository.save(new Course());
         LOGGER.info("HELLO FROM EXAMPLECOMPONENT");
     }
 }
---
+++ src/main/java/com/udemy/backendninja/controller/repository/CourseJpaRepository.java
@@ -0,0 +1,18 @@
+package com.udemy.backendninja.controller.repository;
+
+import com.udemy.backendninja.controller.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Repository("courseJpaRepository")
+public interface CourseJpaRepository extends JpaRepository<Course, Serializable> {
+
+    public abstract Course findByPrice(int price);
+    public abstract Course findByPriceAndName(int price, String name);
+    public abstract List<Course> findByNameOrderByHours(String name);
+    public abstract Course findByNameOrPrice(String name, int price);
+    
+}
